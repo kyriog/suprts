2d22dcdb742065af7a8d1c96736fc31a9ea73364
Cyril Glapa <cyril.glapa@keuse.fr>
2013-05-09 16:30:19 +0200

    Removing database config file from repo and adding an example config file

--- ServerConfig.default.js
+++ ServerConfig.default.js
@@ -6,20 +6,19 @@ var config = {
 		{name: 'Character', path: './gameClasses/Entities/Character'},
 		{name: 'Chunk', path: './gameClasses/World/Chunk'},
 		{name: 'Title', path: './gameClasses/World/Title'},
-		{name: 'extend', path: './gameClasses/utils/extend'},
+		{name: 'extend', path: './gameClasses/Utils/extend'},
 		{name: 'WorldGenerator', path: './gameClasses/World/WorldGenerator'},
 		{name: 'AttackAction', path: './gameClasses/PlayerAction/AttackAction'},
 		{name: 'FertilizeAction', path: './gameClasses/PlayerAction/FertilizeAction'},
 		{name: 'HarvestAction', path: './gameClasses/PlayerAction/HarvestAction'},
 		{name: 'WateringAction', path: './gameClasses/PlayerAction/WateringAction'},
 		{name: 'PlantAction', path: './gameClasses/PlayerAction/PlantAction'},
-		//{name: 'DatabaseManager', path: './gameClasses/Database/DatabaseManager'}
 	],
 	db: {
 		type: 'mysql',
 		host: 'localhost',
 		user: 'root',
-		pass: 'mathieu59',
+		pass: '',
 		dbName: 'suprts'
 	}
 };
