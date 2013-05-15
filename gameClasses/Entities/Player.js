45f95ad41932dd7a2138e5da854569871023b996
Mathieu Jaeger <93739@supinfo.com>
2013-05-15 15:43:53 +0200

    Added StreamingSectionData for Level and Money

--- gameClasses/Entities/Player.js
+++ gameClasses/Entities/Player.js
@@ -1,4 +1,4 @@
-// Define our player character container classes
+// Define our player
 var Player = IgeEntity.extend(
 {
 	classId: 'Player',
@@ -7,6 +7,12 @@ var Player = IgeEntity.extend(
 	{
 		var self = this;
 		IgeEntity.prototype.init.call(this);
+		this.streamSections(['transform', 'money', 'level']);
+		
+		this.money = 0;
+		this.level = 0;
+		
+		this.guiCallback; // Proto: guiCallback(attributeName, value);
 		
 		this.drawBounds(false);
 		this.drawBoundsData(false);
@@ -19,6 +25,44 @@ var Player = IgeEntity.extend(
 		self.character = new Character().id(this.id() + '_character').setType(3).drawBounds(false).drawBoundsData(false).originTo(0.5, 0.6, 0.5).mount(this);
 	},
 
+	streamSectionData: function (sectionId, data) 
+	{
+		if (sectionId === 'money') 
+		{
+			if (data) 
+			{				
+				if(!ige.isServer && this.money != data)
+				{
+					this.money = data;
+					this.guiCallback('money',this.money); // for update GUI
+				}
+			} 
+			else 
+			{
+				return this.money;
+			}
+		} 
+		else if(sectionId == 'level')
+		{
+			if (data) 
+			{
+
+				if(!ige.isServer && this.level != data)
+				{
+					this.level = data;
+					this.guiCallback('level',this.level); // for update GUI
+				}
+			} 
+			else 
+			{
+				return this.level;
+			}
+		}
+		else 
+		{
+			return IgeEntity.prototype.streamSectionData.call(this, sectionId, data);
+		}
+	},
 	/**
 	 * Tweens the character to the specified world co-ordinates.
 	 * @param x
@@ -107,6 +151,8 @@ var Player = IgeEntity.extend(
 		this.depth(this._translate.y);
 		IgeEntity.prototype.tick.call(this, ctx);
 	}
+
+	
 });
 
 if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = Player; }
